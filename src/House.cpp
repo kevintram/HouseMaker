01372f3357bb3c9c03044fad8dbe894f39d20dbf
kevintram <thekevintram@gmail.com>
2021-02-21 14:04:07 -0500

    Implements House

---
+++ src/House.cpp
@@ -0,0 +1,19 @@
+#include "House.h"
+#include <iostream>
+
+using namespace std;
+
+House::House(int bathroomCapacity, int bedroomCapacity) {
+    this->bathroomCapacity = bathroomCapacity;
+    this->bedroomCapacity = bedroomCapacity;
+}
+
+void House::print() {
+    for (Bathroom bathroom : bathrooms) {
+        bathroom.print();
+    }
+}
+
+void House::addBathroom(const Bathroom& bathroom) {
+    bathrooms.push_back(bathroom);
+}
--- src/House.h
+++ src/House.h
@@ -1,13 +1,19 @@
 #pragma once
 
 #include "amenities/Bathroom.h"
+#include <vector>
+
+using namespace std;
 
 class House {
     public:
         House(int bathroomCapacity = 2, int bedroomCapacity = 3);
         void print();
+
+        void addBathroom(const Bathroom& bathroom);
     private:
         int bathroomCapacity;
         int bedroomCapacity;
-        void addBathroom(Bathroom bathroom);
+
+        vector<Bathroom> bathrooms;
 };
