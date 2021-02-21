a56c54c07f42b1daa17a69702ee8a0c13d095eaa
kevintram <thekevintram@gmail.com>
2021-02-21 16:31:58 -0500

    Moves bathroom definition to header file and adds getters for all furniture

--- src/amenities/Bathroom.h
+++ src/amenities/Bathroom.h
@@ -1,6 +1,7 @@
 #pragma once 
 
 #include <string>
+#include <iostream>
 #include "furniture/Toilet.h"
 #include "furniture/Shower.h"
 #include "furniture/Sink.h"
@@ -8,15 +9,46 @@
 using namespace std;
 
 class Bathroom {
-    public: 
-        Bathroom(string name = "Bathroom");
-        void setToilet(Toilet toilet);
-        void setShower(Shower shower);
-        void setSink(Sink sink);
-        void print();
     private:
         string name; 
-        Toilet toilet;
-        Shower shower;
-        Sink sink;
+        Toilet* toilet;
+        Shower* shower;
+        Sink* sink;
+
+    public: 
+        inline Bathroom(string name = "Bathroom") {
+            this->name = name;
+        }
+
+        inline void setToilet(Toilet* toilet) {
+            this->toilet = toilet;
+        }
+
+        inline void setShower(Shower* shower) {
+            this->shower = shower;
+        }
+
+        inline void setSink(Sink* sink) {
+            this->sink = sink;
+        }
+
+        inline Toilet* getToilet() {
+            return this->toilet;
+        }
+
+        inline Shower* getShower() {
+            return this->shower;
+        }
+
+        inline Sink* getSink() {
+            return this->sink;
+        }
+
+        void print() {
+            cout << name << endl;
+            toilet->print();
+            shower->print();
+            sink->print();
+        }
+
 };
