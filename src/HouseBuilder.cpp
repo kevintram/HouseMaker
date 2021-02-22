111c5358ea48889b0da2edb39d535f72f329194c
kevintram <thekevintram@gmail.com>
2021-02-21 19:15:56 -0500

    Adds all amenities to HouseBuilder

--- src/HouseBuilder.cpp
+++ src/HouseBuilder.cpp
@@ -30,7 +30,47 @@ HouseBuilder& HouseBuilder::addBathroom(const Bathroom& bathroom) {
     return *this;
 }
 
+HouseBuilder& HouseBuilder::addBedroom(const Bedroom& bedroom) {
+    house->addBedroom(bedroom);
+    return *this;
+}
+
+HouseBuilder& HouseBuilder::setGarage(const Garage& garage) {
+    house->setGarage(garage);
+    return *this;
+}
+
+HouseBuilder& HouseBuilder::setGarden(const Garden& garden) {
+    house->setGarden(garden);
+    return *this;
+}
+
 HouseBuilder& HouseBuilder::setKitchen(const Kitchen& kitchen) {
     house->setKitchen(kitchen);
+    return *this;
+}
+
+HouseBuilder& HouseBuilder::setLivingRoom(const LivingRoom& livingRoom) {
+    house->setLivingRoom(livingRoom);
+    return *this;
+}
+
+HouseBuilder& HouseBuilder::setOffice(const Office& office) {
+    house->setOffice(office);
+    return *this;
+}
+
+HouseBuilder& HouseBuilder::setPlayground(const Playground& playground) {
+    house->setPlayground(playground);
+    return *this;
+}
+
+HouseBuilder& HouseBuilder::setPool(const Pool& pool) {
+    house->setPool(pool);
+    return *this;
+}
+
+HouseBuilder& HouseBuilder::setReadingRoom(const ReadingRoom& readingRoom) {
+    house->setReadingRoom(readingRoom);
     return *this;
 }
--- src/HouseBuilder.h
+++ src/HouseBuilder.h
@@ -2,7 +2,15 @@
 
 #include "House.h"
 #include "amenities/Bathroom.h"
+#include "amenities/Bedroom.h"
+#include "amenities/Garage.h"
+#include "amenities/Garden.h"
 #include "amenities/Kitchen.h"
+#include "amenities/LivingRoom.h"
+#include "amenities/Office.h"
+#include "amenities/Playground.h"
+#include "amenities/Pool.h"
+#include "amenities/ReadingRoom.h"
 
 class HouseBuilder {
     public:
@@ -14,7 +22,15 @@ class HouseBuilder {
         HouseBuilder& reset();
 
         HouseBuilder& addBathroom(const Bathroom& bathroom);
+        HouseBuilder& addBedroom(const Bedroom& bedroom);
+        HouseBuilder& setGarage(const Garage& garage);
+        HouseBuilder& setGarden(const Garden& garden);
         HouseBuilder& setKitchen(const Kitchen& kitchen);
+        HouseBuilder& setLivingRoom(const LivingRoom& livingRoom);
+        HouseBuilder& setOffice(const Office& office);
+        HouseBuilder& setPlayground(const Playground& playground);
+        HouseBuilder& setPool(const Pool& pool);
+        HouseBuilder& setReadingRoom(const ReadingRoom& readingRoom);  
 
         House& build();
     private: 
