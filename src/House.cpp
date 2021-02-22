cfd7e7015c36ba522bcf6cd91df8d807096ee7d8
kevintram <thekevintram@gmail.com>
2021-02-21 19:22:43 -0500

    Adds all amenities to House

--- src/House.cpp
+++ src/House.cpp
@@ -18,6 +18,38 @@ void House::addBathroom(const Bathroom& bathroom) {
     bathrooms.push_back(bathroom);
 }
 
+void House::addBedroom(const Bedroom& bedroom) {
+    bedrooms.push_back(bedroom);
+}
+
+void House::setGarage(const Garage& garage) {
+    this->garage = garage;
+}
+
+void House::setGarden(const Garden& garden) {
+    this->garden = garden;
+}
+
 void House::setKitchen(const Kitchen& kitchen) {
     this->kitchen = kitchen;
+}
+
+void House::setLivingRoom(const LivingRoom& livingRoom) {
+    this->livingRoom = livingRoom;
+}
+
+void House::setOffice(const Office& office) {
+    this->office = office;
+}
+
+void House::setPlayground(const Playground& playground) {
+    this->playground = playground;
+}
+
+void House::setPool(const Pool& pool) {
+    this->pool = pool;
+}
+
+void House::setReadingRoom(const ReadingRoom& readingRoom) {
+    this->readingRoom = readingRoom;
 }
--- src/House.h
+++ src/House.h
@@ -1,7 +1,15 @@
 #pragma once
 
 #include "amenities/Bathroom.h"
+#include "amenities/Bedroom.h"
+#include "amenities/Garage.h"
+#include "amenities/Garden.h"
 #include "amenities/Kitchen.h"
+#include "amenities/LivingRoom.h"
+#include "amenities/Office.h"
+#include "amenities/Playground.h"
+#include "amenities/Pool.h"
+#include "amenities/ReadingRoom.h"
 #include <vector>
 
 using namespace std;
@@ -12,11 +20,28 @@ class House {
         void print();
 
         void addBathroom(const Bathroom& bathroom);
+        void addBedroom(const Bedroom& bedroom);
+        void setGarage(const Garage& garage);
+        void setGarden(const Garden& garden);
         void setKitchen(const Kitchen& kitchen);
+        void setLivingRoom(const LivingRoom& livingRoom);
+        void setOffice(const Office& office);
+        void setPlayground(const Playground& playground);
+        void setPool(const Pool& pool);
+        void setReadingRoom(const ReadingRoom& readingroom);
+
     private:
         int bathroomCapacity;
         int bedroomCapacity;
 
         vector<Bathroom> bathrooms;
+        vector<Bedroom> bedrooms;
+        Garage garage;
+        Garden garden;
         Kitchen kitchen;
+        LivingRoom livingRoom;
+        Office office;
+        Playground playground;
+        Pool pool;
+        ReadingRoom readingRoom;
 };
