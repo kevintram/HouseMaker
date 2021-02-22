4dcb30ff066c14f1728ea73123865765faf004d9
kevintram <thekevintram@gmail.com>
2021-02-22 11:52:53 -0500

    Adds build directory

---
+++ build/CMakeFiles/HouseMaker.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/HouseMaker.dir/src/GenericHouseBuilder.cpp.o"
+  "CMakeFiles/HouseMaker.dir/src/House.cpp.o"
+  "CMakeFiles/HouseMaker.dir/src/HouseMaker.cpp.o"
+  "HouseMaker"
+  "HouseMaker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/HouseMaker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
